65cbdbd616dcd50133bc8edc4a577f7323172ce7
fonsik2 <alfat.salikhov@gmail.com>
2024-02-05 09:44:38 +0300

    manual db update

---
+++ src/main/java/com/pet/depo23/Depo23Application.java
@@ -0,0 +1,13 @@
+package com.pet.depo23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Depo23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Depo23Application.class, args);
+    }
+
+}
